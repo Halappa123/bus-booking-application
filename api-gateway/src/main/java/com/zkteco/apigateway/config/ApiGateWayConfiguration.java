6094fead690396ebb949b90fb0b1be993e1621c3
Halappa <halappanm03@gmail.com>
2023-11-22 18:34:05 +0530

    service added

---
+++ api-gateway/src/main/java/com/zkteco/apigateway/config/ApiGateWayConfiguration.java
@@ -0,0 +1,32 @@
+package com.zkteco.apigateway.config;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+@Configuration
+public class ApiGateWayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        return  builder.routes()
+                .route(p->p.path("/get")
+                        .filters(f->f.addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p->p.path("/api/**")
+                        .uri("lb://bus-service"))
+                .route(p->p.path("/api/user_login/**")
+                        .uri("lb://user-service"))
+//                .route(p->p.path("/api/**")
+//                        .uri("lb://user-service"))
+                .build();
+    }
+}
