4899edede2bb24262b9cb50e21480a18ca57469f
Halappa <halappanm03@gmail.com>
2023-11-24 18:28:04 +0530

    last change of the day

---
+++ user-service/src/main/java/com/zkteco/userservice/controller/CircuitBreakerController.java
@@ -0,0 +1,30 @@
+package com.zkteco.userservice.controller;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger= LoggerFactory.getLogger(CircuitBreakerController.class);
+
+
+    @GetMapping("/sample-api")
+ //   @Retry(name = "sample-api",fallbackMethod="hardCodedResponse")
+    @CircuitBreaker(name = "default",fallbackMethod="hardCodedResponse")
+    public String sampleApi(){
+        logger.info("sample api call received");
+        ResponseEntity<String> forEntity= new RestTemplate().getForEntity("http://localhost:8100/bus-api", String.class);
+        return forEntity.getBody();
+//        return "Sample Api";
+    }
+    public String hardCodedResponse(Exception ex){
+        return "Fallback-response";
+    }
+}
