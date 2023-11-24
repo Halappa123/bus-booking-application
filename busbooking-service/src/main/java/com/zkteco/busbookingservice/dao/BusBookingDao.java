23acdb82082c45c0be6ff653b1c4c74685b2b8cd
Halappa <halappanm03@gmail.com>
2023-11-24 15:31:10 +0530

    changes commited

--- busbooking-service/src/main/java/com/zkteco/busbookingservice/dao/BusBookingDao.java
+++ busbooking-service/src/main/java/com/zkteco/busbookingservice/dao/BusBookingDao.java
@@ -1,12 +1,21 @@
 package com.zkteco.busbookingservice.dao;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(value = "bus-booking-table")
 public class BusBookingDao {
 
+    @Id
     private String bookingId;
+
+
     private Long userId;
 
     private String busId;
+    private int seats;
+    private Long bookingTime;
+
+    private String status;
+
 }
